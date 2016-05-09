671868c4c73989d585ef80075be661057bb5c6af
FlynnMcG <fmcge001@ucr.edu>
2016-05-09 15:44:02 -0700

    Added some menu.h functions

---
+++ menu.cpp
@@ -0,0 +1,34 @@
+
+#include "menu.h"
+
+using namespace std;
+
+int history_index;
+vector<Command*> history;
+Menu::Menu() {
+    history_index=0;
+	//Base constructor to set up all necessary members
+};
+void Menu::execute() {
+	//Prints the current commands value (based on history_index), if no commands exist
+	//print 0 by default
+};
+bool Menu::initialized() {
+	//Return true if the history has been primed with a single op instruciton
+	//This is necessary because that is the base of a calculation
+};
+void Menu::add_command(Command* cmd) {
+    vector.at(history_index)=cmd;
+    history_index++;
+	//Adds a command to history in the appropriate posiiton (based on history_index)
+};
+Command* Menu::get_command() {
+	//Returns the command in history we are currently referring to (based on history_index)
+};
+void Menu::undo() {
+	//Moves to the last command in history (if possible)
+};
+void Menu::redo() {
+	//Moves to the next command in history (if possible)
+};
+
--- menu.h
+++ menu.h
@@ -18,21 +18,47 @@ class Menu {
 		void execute() {
 			//Prints the current commands value (based on history_index), if no commands exist
 			//print 0 by default
+			if(initialized())
+			{
+				cout<<history.at(history_index)->execute();
+			}
+			else
+				cout<<'0';
 		};
 		bool initialized() {
+			if(history.size()!=0)
+			{
+				return true;
+			}
+			else
+				return false;
 			//Return true if the history has been primed with a single op instruciton
 			//This is necessary because that is the base of a calculation
 		};
 		void add_command(Command* cmd) {
+			history.push_back(cmd);
+			history_index++;
 			//Adds a command to history in the appropriate posiiton (based on history_index)
 		};
 		Command* get_command() {
+			if(initialized())
+			{
+				return history.at(history_index);
+			}
 			//Returns the command in history we are currently referring to (based on history_index)
 		};
 		void undo() {
+			if(history_index!=0)
+			{
+				history_index--;
+			}
 			//Moves to the last command in history (if possible)
 		};
 		void redo() {
+			if(history_index!=history.size()-1)
+			{	
+				history_index++;
+			}
 			//Moves to the next command in history (if possible)
 		};
 };
